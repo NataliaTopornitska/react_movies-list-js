f5b6ac9e251c2462345d6d49a2efb15db203a674
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-07-17 14:33:46 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </div>
+);
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
